1267f55d7c9df7ecd2caedacfd2448132c0410ae
José Luis Abellán Monreal <joseluisabellanmonreal@gmail.com>
2025-03-03 20:12:51 +0100

    Usuario

--- server/index.js
+++ server/index.js
@@ -1,35 +1,98 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { UserRepository } from '../user-repository.js'
+import { createClient } from '@libsql/client'
 
 dotenv.config()
 
 const app = express()
 
-app.use(express.json())
+app.set('view engine', 'ejs')
+app.set('views', './public/views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true })) // Middleware para procesar datos del formulario
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+
+  req.session = { user: null }
+  try {
+    const data = jwt.verify(token, process.env.SECRET_JWT_KEY)
+    req.session.user = data
+  } catch (error) {
+    req.session.user = null
+  }
+
+  next() // --> seguir con la ejecución
+})
 
 const port = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {
-  const { username, password } = req.body
-  console.log(req.body)
-
-  try {
-    const id = UserRepository.create({ username, password })
-    res.send({ id })
-  } catch (error) {
-    // Arreglar tratamiento del error
-    res.status(400).send(error.message)
+  const { user } = req.session
+  if (user) {
+    res.render('protected', { username: user.username })
+  } else {
+    res.render('login')
   }
 })
-app.post('/logout', (req, res) => {})
 
-app.get('/protected', (req, res) => {})
+app.get('/login', (req, res) => res.render('login'))
+app.get('/register', (req, res) => res.render('register'))
+
+app.post('/register', async (req, res) => {
+  const { email, username, password } = req.body
+
+  try {
+    const id = await UserRepository.create({ email, username, password })
+    res.redirect('/protected')
+  } catch (error) {
+    // Arreglar tratamiento del error
+    res.status(400).send('<h1>Error 400: Bad request</h1>')
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await UserRepository.login({ email, password })
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.SECRET_JWT_KEY,
+      { expiresIn: '1h' }
+    )
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Solo se envía en HTTPS
+        sameSite: 'strict', // Solo se envía si la petición es del mismo sitio
+        maxAge: 1000 * 60 * 60 // 1 hora
+      })
+      .redirect('/protected')
+  } catch (error) {
+    res.redirect('/error')
+  }
+})
+
+app.get('/error', (req, res) => {
+  res.render('error', { errorcode: 401 })
+})
+
+app.get('/logout', (req, res) => {
+  const { user } = req.session
+  if (!user) return res.redirect('/login')
+  res.clearCookie('access_token').send('Logged out')
+})
+
+app.get('/protected', async (req, res) => {
+  // TODO: Verificar token
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized')
+  res.render('protected', { username: user.username })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
--- user-repository.js
+++ user-repository.js
@@ -1,36 +1,73 @@
+import crypto from 'node:crypto'
 import DBLocal from 'db-local'
-import path from 'path'
-import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import zod from 'zod'
 
 const { Schema } = DBLocal({ path: './db' })
 
 const User = Schema('User', {
   _id: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true }
 })
 
+const usuarioSchema = zod.object({
+  email: zod
+    .string()
+    .regex(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/, 'Email inválido'),
+  username: zod
+    .string()
+    .max(20, 'El nombre de usuario no puede tener más de 20 caracteres')
+    .optional(),
+  password: zod
+    .string()
+    .min(6, 'La contraseña debe tener al menos 6 caracteres')
+})
 export class UserRepository {
-  static create({ username, password }) {
+  static async create({ email, username, password }) {
     // 1. Comprobamos que los datos sean correctos
-    if (username !== typeof String || password !== typeof String)
-      throw new Error('Invalid input, password and username must be a string')
-    if (username.length < 3)
-      throw new Error('Username must be at least 3 characters long')
-    if (password.length < 6)
-      throw new Error('Password must be at least 6 characters long')
+    const res = usuarioSchema.safeParse({ email, username, password })
+    if (!res.success) {
+      throw new Error(res.error.errors[0].message)
+    }
+    //  2. Comprobamos que el usuario no existe
 
-    //2. Comprobamos que el usuario no existe
-
-    const user = User.findOne({ username })
+    const user = User.findOne({ email })
+    if (user) {
+      throw new Error('E-mail already exists')
+    }
     const id = crypto.randomUUID()
+    const hashedPassword = await bcrypt.hash(password, 10) // Devuelve una promesa
 
     User.create({
       _id: id,
+      email,
       username,
-      password
+      password: hashedPassword
     }).save()
 
     return id
   }
-  static login({ username, password }) {}
+
+  static async login({ email, password }) {
+    const res = usuarioSchema.safeParse({ email, password })
+    if (!res.success) {
+      throw new Error(res.error.errors[0].message)
+    }
+
+    const user = User.findOne({ email })
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) {
+      throw new Error('Invalid password')
+    }
+
+    const { password: _, ...publicUser } = user // Forma limpia de omitir un campo
+
+    return publicUser
+  }
 }
